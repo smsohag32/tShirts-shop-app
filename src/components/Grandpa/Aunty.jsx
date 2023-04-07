ae65cfaa725203afcf185d849e8fecf955b0e68f
SM Sohag <sohagsheik32@gmail.com>
2023-04-07 16:06:56 +0600

    context hook impliment

--- src/components/Grandpa/Aunty.jsx
+++ src/components/Grandpa/Aunty.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Cousin from "../Uncle/Cousin";
+import { Diamond } from "./Grandpa";
 
 const Aunty = () => {
+  const gold = useContext(Diamond);
+  console.log(gold);
   return (
     <div>
       <p>Anty</p>
       <section>
+        <div>
+          <p>{gold}</p>
+        </div>
         <Cousin />
       </section>
     </div>
--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import Uncle from "../Uncle/Uncle";
 import "./Grandpa.css";
 import Fathar from "../Fathar/Fathar";
 import Aunty from "./Aunty";
+
+export const Diamond = createContext(null);
+// components
 const Grandpa = () => {
+  // prop drilling massage
   const ring = "dimond";
   return (
-    <div className="grandpa">
-      <p>Grandpa</p>
-      <section className="flex">
-        <Uncle />
-        <Fathar ring={ring} />
-        <Aunty />
-      </section>
-    </div>
+    <Diamond.Provider value={"gold"}>
+      <div className="grandpa">
+        <p>Grandpa</p>
+        <section className="flex">
+          <Uncle />
+          <Fathar ring={ring} />
+          <Aunty />
+        </section>
+      </div>
+    </Diamond.Provider>
   );
 };
 
